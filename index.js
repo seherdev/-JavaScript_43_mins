d41b471284ec632557092e431414e8313da5313c
seherdev <7iletisimseherarslanbay@gmail.com>
2025-08-24 23:30:36 +0300

    1

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log('helllo worllddd') //neden " " içinde değil?
+console.log("hello worldddd") //fark var mı ?
+//araştırdım --> 
+/*
+const a = "Ahmet'in kalemi"; // içinde ' varsa, dışarıda " iyi olur
+const b = 'She said "hello"'; // içinde " varsa, dışarıda ' iyi olur
+-------YA DA-------
+const name = 'Seher';
+console.log(`Merhaba, ${name}!`);
+--YANİ
+**Backtick (` `) ile yazılır.
+**Değişken içeriği ${} ile string’e gömülebilir.
+**Çok satırlı string yazılabilir.
+
+*/
