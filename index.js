89e76e28de178867a69f172eb30e75f4dc8ad2c4
seherdev <7iletisimseherarslanbay@gmail.com>
2025-08-24 23:54:19 +0300

    2

--- index.js
+++ index.js
@@ -13,3 +13,38 @@ console.log(`Merhaba, ${name}!`);
 **Çok satırlı string yazılabilir.
 
 */
+
+
+//let keyword is better for variable
+let name = "Mosh";
+console.log(name);
+
+//ts dynamic bir dil!!!!!!
+
+//OBJECTS
+let person = {
+  name: "Mosh",
+  age:30
+};
+
+//dot notation    //bu daha kısa ve daha anlaşılır dedi
+console.log(person)
+
+//bracket notation
+let selection = 'name';
+person['name'] = 'Mary';
+
+console.log(person.name);
+
+//ARRAYS
+let selectedColors = ["red", "blue"];
+
+selectedColors[2] = "green";
+selectedColors[2] = 1;
+console.log(selectedColors);
+
+console.log(selectedColors.length) //kaç tane element var içinde mesela
+// -----------------------. koyup altta çıkanlara bak. hepsini kullanabilrsin
+
+//--------functions----------
+//bunu diğer 60mins ya da diğer videoda daha iyi gördük
